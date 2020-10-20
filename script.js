5c9882c5128d2028c701a395c92b706e3d605b86
hamanenergyindonesia <73122231+hamanenergyindonesia@users.noreply.github.com>
2020-10-20 21:00:13 +0700

    hamanenergyindonesia v5
    mobile friendly (?) adding hamburger menu.

--- script.js
+++ script.js
@@ -17,5 +17,5 @@
 	
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+    document.getElementsByClassName("topnav1")[0].classList.toggle("responsive");
 }
