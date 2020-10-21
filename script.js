9e09fe7150902f488d71a628e64b34f6e6a21517
hamanenergyindonesia <73122231+hamanenergyindonesia@users.noreply.github.com>
2020-10-21 13:58:08 +0700

    v6
    almost fix all ^^

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 		if
       ($(document).scrollTop() > 100){
 		  $("header ul a img").addClass("shrink");
-		  $("header ul li a").addClass("shrink1");
-		  $("header h1-2").addClass("shrink2");
+		  $("header ul.topnav li a").addClass("shrink1");
+		  
+		  
 		}
 		else
 		{
 			$("header ul a img").removeClass("shrink");
-			$("header ul li a").removeClass("shrink1");
-			$("header h1-2").removeClass("shrink2");
+			$("header ul.topnav li a").removeClass("shrink1");
 		}
 	}
 	);
@@ -18,4 +18,5 @@
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     document.getElementsByClassName("topnav1")[0].classList.toggle("responsive");
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive0");
 }
